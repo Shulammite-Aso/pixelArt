3e529783eec93a490282597fb613cfa9babfa538
Shulammite-Aso <asoamarachi@gmail.com>
2022-07-13 08:52:43 -0700

    add creat new file(image) functionality

--- ui/layout.go
+++ ui/layout.go
@@ -6,6 +6,7 @@ import (
 
 // Will setup all of our different ui elements
 func Setup(app *AppInit) {
+	SetupMenus(app)
 	swatchesContainer := BuildSwatches(app)
 	colorpicker := SetupColorPicker(app)
 	appLayout := container.NewBorder(nil, swatchesContainer, nil, colorpicker, app.PixelArtCanvas)
